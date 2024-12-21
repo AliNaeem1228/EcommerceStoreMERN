035f01b8cc0e89590f1304f3e8e270d06fa9e506
Muhammad Ali Naeem <alinaeemgohan1228@yahoo.com>
2024-12-21 14:00:18 +0500

    changes

--- frontend/src/components/SuccessMsg/SuccessMsg.js
+++ frontend/src/components/SuccessMsg/SuccessMsg.js
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
 import Swal from "sweetalert2";
 
 const SuccessMsg = ({ message }) => {
-  Swal.fire({
-    icon: "success",
-    title: "Good job!",
-    text: message,
-  });
+  useEffect(() => {
+    if (message) {
+      Swal.fire({
+        icon: "success",
+        title: "Good job!",
+        text: message,
+      });
+    }
+  }, [message]); // Run this effect only when `message` changes
+
+  return null; // This component does not render anything visually
 };
 
 export default SuccessMsg;
